cf674e07b399258d9fe78295a234718dbb16eef0
francescacosta <francescacosta@francescasimac2.home>
2017-08-10 16:59:04 +1000

    attempt to get the up arrow to go up through increase timer and o'clock functions

--- javascript.js
+++ javascript.js
@@ -21,5 +21,15 @@ timerCancelButton.onclick = function() {
 }
 
 // POMODORO TIMER
-var upArrow = document.getElementsByClassName('arrow1')[0];
-var downArrow =
+var upArrow = document.getElementsByClassName('pomodoro-arrow-up')[0];
+var downArrow = document.getElementsByClassName('pomodoro-arrow-down')[0];
+var pomodoroTimer = '0'
+
+var increaseTimer = function(value) {
+   pomodoroTimer +=1;
+}
+
+upArrow.onclick = function() {
+  increaseTimer();
+  console.log(pomodoroTimer);
+}
