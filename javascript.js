c9b35e358ce133523f0b4b52b7ec41296a627a3f
francescacosta <francescacosta@francescasimac2.home>
2017-08-10 14:56:08 +1000

    create var and functions to clear timer

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@ var settingsButton = document.getElementsByClassName('settings-cog')[0];
 var appScreen = document.getElementsByClassName('app-display')[0];
 var settingsScreen = document.getElementsByClassName('settings-display')[0];
 var backButton = document.getElementsByClassName('back-text')[0];
+var timerCancelButton = document.getElementsByClassName('timer-cancel-button')[0];
+var timer =  document.getElementsByClassName('countdown')[0];
 
 settingsButton.onclick = function() {
   appScreen.style.display = "none";
@@ -12,4 +14,8 @@ settingsButton.onclick = function() {
 backButton.onclick = function() {
   appScreen.style.display = "unset";
   settingsScreen.style.display = "none";
+}
+
+timerCancelButton.onclick = function() {
+  timer.innerHTML = "00:00";
 }
