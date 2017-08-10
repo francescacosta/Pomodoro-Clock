7c81b0d18de37d4bbad6b2db88a1bd5a052f8805
francescacosta <francescacosta@francescasimac2.home>
2017-08-10 14:26:18 +1000

    create var and functions for settings and back button

--- javascript.js
+++ javascript.js
@@ -1,10 +1,15 @@
 //Variables
-var settingsButton = document.getElementsByClassName('settings-cog');
-var appScreen = document.getElementsByClassName('app');
-var settingsScreen = document.getElementsByClassName('app-settings');
+var settingsButton = document.getElementsByClassName('settings-cog')[0];
+var appScreen = document.getElementsByClassName('app')[0];
+var settingsScreen = document.getElementsByClassName('app-settings')[0];
+var backButton = document.getElementsByClassName('back-text')[0];
 
 settingsButton.onclick = function() {
   appScreen.style.display = "none";
   settingsScreen.style.display = "unset";
+}
 
+backButton.onclick = function() {
+  appScreen.style.display = "unset";
+  settingsScreen.style.display = "none";
 }
