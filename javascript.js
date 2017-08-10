aaa5cc27caa8568bfab53b8a2650805c6ebb5c0b
francescacosta <francescacosta@francescasimac2.home>
2017-08-10 14:36:08 +1000

    bug fix: display error

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 //Variables
 var settingsButton = document.getElementsByClassName('settings-cog')[0];
-var appScreen = document.getElementsByClassName('app')[0];
-var settingsScreen = document.getElementsByClassName('app-settings')[0];
+var appScreen = document.getElementsByClassName('app-display')[0];
+var settingsScreen = document.getElementsByClassName('settings-display')[0];
 var backButton = document.getElementsByClassName('back-text')[0];
 
 settingsButton.onclick = function() {
